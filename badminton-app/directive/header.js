ebf518057f51e16f1b11bd6e68579a80987b371a
Keerthan V <keerthankumar93@gmail.com>
2017-10-14 00:24:57 +0530

    1. Removed header content from each html views. 2.  Added new directive called header 3.  Added header contents of views in header-view.html 4. Called header-view.html using templateUrl in directive

---
+++ badminton-app/directive/header.js
@@ -0,0 +1,8 @@
+angular.module('badminton').directive('header', function () {
+    'use strict'
+    return {
+        scope:true,
+        templateUrl: 'directive/header-view.html'
+    };
+    
+})
