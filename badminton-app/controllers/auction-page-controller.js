84d08b7a0707e428f092af4d6e3a3ec3ff9d14d5
Keerthan V <keerthankumar93@gmail.com>
2017-10-15 00:33:44 +0530

    added bid board to auction page.

--- badminton-app/controllers/auction-page-controller.js
+++ badminton-app/controllers/auction-page-controller.js
@@ -1,4 +1,4 @@
-angular.module('badminton').controller('auctionPageController', function ($scope, $rootScope, $state, $uibModal, $stateParams, $http, $game,usSpinnerService) {
+angular.module('badminton').controller('auctionPageController', function ($scope, $timeout, $rootScope, $state, $uibModal, $stateParams, $http, $game, usSpinnerService) {
     'use strict'
 
 
@@ -9,6 +9,27 @@ angular.module('badminton').controller('auctionPageController', function ($scope
     $scope.remainingPlayers = {};
     $scope.progressValue1 = "100%";
     $scope.progressValue2 = "100%";
+    $scope.bidPrice = 100000;
+    $scope.slabValue = 100000;
+    $scope.message = false;
+    $scope.isBulldozerOwner = true;
+    // $scope.counter = 30;
+    // var stopped;
+
+    //timeout function
+    //1000 milliseconds = 1 second
+    //Every second counts
+    //Cancels a task associated with the promise. As a result of this, the //promise will be resolved with a rejection.  
+    // $scope.countdown = function () {
+
+    //     stopped = $timeout(function () {
+
+    //         $scope.counter--;
+
+    //         $scope.countdown();
+    //     }, 1000);
+    // };
+
 
     $scope.startSpin = function () {
         usSpinnerService.spin('spinner-1');
@@ -16,11 +37,84 @@ angular.module('badminton').controller('auctionPageController', function ($scope
     $scope.stopSpin = function () {
         usSpinnerService.stop('spinner-1');
     }
-    $scope.$on('$viewContentLoaded', function()
-    {
+    $scope.$on('$viewContentLoaded', function () {
         $scope.refresh();
+        if ($rootScope.userData.isOwner && $rootScope.userData.playerId != "manasa-n") {
+            $scope.isBulldozerOwner = false;
+        }
     });
+
+
+
+
+
+    $scope.bidBySmashDroppers = function () {
+        // $scope.countdown();
+        // $bidding.bidByBulldozers.then(function (response) {
+        //     var payload = {
+        //         "seasonId": $scope.seasons.seasonId,
+        //         "teamId":$scope.teams[0],
+        //         "playerId":"",
+        //         "bidType":"",
+        //         "bidValue": "",
+        //         "timestamp":""
+
+        //     }
+            
+    
+
+        //     $scope.startSpin();
+        //     if (response.statusCode) {
+        //         $scope.refresh();
+        //     }
+        //     else {
+        //         error("invalid response");
+        //         $scope.stopSpin();
+        //     }
+        // }, function (error) {
+        //     alert("error while posting bid");
+        //     $scope.stopSpin();
+
+        // })
+        $scope.slabValue = $scope.getBidSlabValue($scope.bidPrice);
+        $scope.bidPrice = $scope.bidPrice + $scope.slabValue;
+        if ($scope.bidPrice != 10100000) {
+            $scope.message = false;
+        }
+        if ($scope.bidPrice == 10100000) {
+            $scope.message = true;
+            $scope.bidPrice = 100000;
+        }
+    }
+    $scope.bidByBulldozers = function () {
+        // $scope.countdown();
+        $scope.slabValue = $scope.getBidSlabValue($scope.bidPrice);
+        $scope.bidPrice = $scope.bidPrice + $scope.slabValue;
+        if ($scope.bidPrice != 10100000) {
+            $scope.message = false;
+        }
+        if ($scope.bidPrice == 10100000) {
+            $scope.message = true;
+            $scope.bidPrice = 100000;
+        }
+        
+
+    }
+    $scope.getBidSlabValue = function (price) {
+        if (price >= 10000000) {
+            return 100000
+        }
+        if (price < 1000000) {
+            return 100000;
+        } else if (price >= 1000000 && price < 5000000) {
+            return 500000;
+
+        } else if (price >= 5000000) {
+            return 1000000;
+        }
+    }
     $scope.refresh = function () {
+
 
         $game.getAuctionFeed().then(function (response) {
             $scope.startSpin();
@@ -46,19 +140,19 @@ angular.module('badminton').controller('auctionPageController', function ($scope
 
                 $scope.seasons = response.body;
                 $scope.teams = $scope.seasons.teams;
-                
+
                 $scope.progressValue1 = ($scope.teams[0].remainingBudget / $scope.seasons.totalBudget) * 100;
                 document.getElementsByClassName("team1")[0].style.width = $scope.progressValue1 + "%";
                 $scope.progressValue2 = ($scope.teams[1].remainingBudget / $scope.seasons.totalBudget) * 100;
-                document.getElementsByClassName("team2")[0].style.width = $scope.progressValue2+ "%";
+                document.getElementsByClassName("team2")[0].style.width = $scope.progressValue2 + "%";
                 $scope.remainingPlayers = $scope.seasons.remainingPlayers;
-               $scope.stopSpin();
-                
+                $scope.stopSpin();
+
             }
             else {
                 error("invalid response");
-                  $scope.stopSpin();
-                
+                $scope.stopSpin();
+
             }
         }, function (error) {
             alert("error while getting seasons teams players");
