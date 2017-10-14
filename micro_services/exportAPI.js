e4dfe1773f53e8e439b0f9b9d6d93d72b7df7f47
Rajath R <rajathr93@gmail.com>
2017-10-14 19:39:14 +0530

    added isOwner column in players table.

--- micro_services/exportAPI.js
+++ micro_services/exportAPI.js
@@ -2,7 +2,7 @@
 
 console.log('Loading function');
 
-var mysql = require('mysql');
+var mysql      = require('mysql');
 
 exports.handler = (event, context, callback) => {
     var connection = mysql.createConnection({
@@ -26,7 +26,7 @@ exports.handler = (event, context, callback) => {
         } else {
             console.log('MySQL Connection obtained');
             var sqlQuery = "SELECT sea.Id AS seasonId, sea.name AS seasonName, sea.overall_budget AS totalBudget, team.Id AS teamId, team.name AS teamName, (sea.overall_budget - SUM(seasons_teams_players.price)) as remainingBudget, player.id AS playerId, player.name AS name, seasons_teams_players.price AS price FROM seasons_teams_players JOIN seasons AS sea ON sea.id = seasons_teams_players.season_id JOIN teams AS team ON team.id = seasons_teams_players.team_id JOIN players AS player ON player.id = seasons_teams_players.player_id GROUP BY sea.Id, team.Id, player.id"
-            var sqlQueryForRemainingPlayers = "SELECT  * FROM players as player WHERE player.id NOT IN (SELECT sea_team_player.player_id FROM seasons_teams_players as sea_team_player)"
+            var sqlQueryForRemainingPlayers = "SELECT  * FROM players as player WHERE player.id NOT IN (SELECT sea_team_player.player_id FROM seasons_teams_players as sea_team_player) and player.is_owner = 0"
             connection.query(sqlQuery, function(err, rows) {
                 var totalBudget = 0;
                 var seasonsTeamsPlayers = {};
