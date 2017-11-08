b6ef32999c498f03adcfd939a6de8843514ec040
Keerthan V <keerthankumar93@gmail.com>
2017-11-08 10:32:47 +0530

    capturing basic score points co-ordinates

--- badminton-app/controllers/match-page-controller.js
+++ badminton-app/controllers/match-page-controller.js
@@ -1,4 +1,4 @@
-angular.module('badminton').controller('matchPageController', function ($scope, $rootScope, $state, $uibModal, $stateParams, $login, usSpinnerService, $localStorage) {
+angular.module('badminton').controller('matchPageController', function ($scope, $rootScope, $state, $uibModal, $stateParams, $login,$timeout, usSpinnerService, $localStorage) {
     'use strict'
 
 
@@ -14,17 +14,18 @@ angular.module('badminton').controller('matchPageController', function ($scope, 
     $scope.serveTeam = "";
     $scope.receiveTeam = "";
     $scope.courtDisable = false;
+    $scope.isScoreSelected = false;
     $scope.gameTrack = [{
         "teamApoints": 0,
         "teamBpoints": 0,
         "activePlayerId": "",
         "teamAplayer1": "",
-        "teamAplayer2":"",
-        "teamBplayer1":"",
-        "teamBplayer2":""
+        "teamAplayer2": "",
+        "teamBplayer1": "",
+        "teamBplayer2": ""
 
     }];
-    
+
     $scope.setActivePlayer = function (id) {
         if (id == "playerA2") {
             if ($scope.teamApoints == 0 && $scope.teamBpoints == 0) {
@@ -32,7 +33,7 @@ angular.module('badminton').controller('matchPageController', function ($scope, 
                 $scope.serveTeam = "teamA";
                 $scope.receiveTeam = "teamB";
                 $scope.activePlayerId = "playerA2";
-                
+
                 $scope.setPlayers();
                 $(".activePlayer").removeClass('activePlayer');
                 $("#playerA2").addClass("activePlayer");
@@ -54,7 +55,7 @@ angular.module('badminton').controller('matchPageController', function ($scope, 
 
     }
 
-    $scope.setPlayers = function (){
+    $scope.setPlayers = function () {
         $scope.gameTrack[0].teamAplayer1 = $scope.playerA1;
         $scope.gameTrack[0].teamAplayer2 = $scope.playerA2;
         $scope.gameTrack[0].teamBplayer1 = $scope.playerB1;
@@ -62,10 +63,10 @@ angular.module('badminton').controller('matchPageController', function ($scope, 
     }
 
     $scope.undo = function () {
-        
-        
+
+
         var len = $scope.gameTrack.length - 1;
-        var activeId = "#"+$scope.gameTrack[len].activePlayerId;
+        var activeId = "#" + $scope.gameTrack[len].activePlayerId;
         $scope.playerA1 = $scope.gameTrack[len].teamAplayer1;
         $scope.playerA2 = $scope.gameTrack[len].teamAplayer2;
         $scope.playerB1 = $scope.gameTrack[len].teamBplayer1;
@@ -74,7 +75,7 @@ angular.module('badminton').controller('matchPageController', function ($scope, 
         len = $scope.gameTrack.length - 1;
         $scope.teamApoints = $scope.gameTrack[len].teamApoints;
         $scope.teamBpoints = $scope.gameTrack[len].teamBpoints;
-        
+
         if (len == 0) {
             $scope.undoDisable = true;
             $scope.courtDisable = false;
@@ -133,8 +134,107 @@ angular.module('badminton').controller('matchPageController', function ($scope, 
         $scope.isStart = true;
 
     }
+    $scope.isSet = false;
+    $scope.isCoordinateSelected = false;
+    $scope.first = false;
+    $scope.second = false;
+    
+    $scope.getCourtCoordinates = function () {
+        
+        
+            $scope.isCoordinateSelected = true;
+            
+
+            $('.boxA').bind('click', function (ev) {
+
+                ev.stopImmediatePropagation();
+                if($scope.first == false){
+                    $scope.first = true;
+                }
+                else{
+                    $scope.second = true;
+                }
+                
+                var offset = $('.court').offset();
+                var x = ev.clientX - offset.left;
+                var y = ev.clientY - offset.top;
+
+                console.log(x, y);
+                 $('#bubble').css('top',y+70);
+                 $('#bubble').css('left',x+10);
+                 $('.boxA').css('opacity','0.5');
+                 $('.boxA').each(function (){
+                    this.style.pointerEvents = 'none'; 
+                 }); 
+                 
+                 if($scope.second == true){
+                    
+                    $scope.restore();
+                  
+                 }
+            });
+            $('.boxB').bind('click',function (ev) {
+                if($scope.first == false){
+                    $scope.first = true;
+                }
+                else{
+                    $scope.second = true;
+                }
+                
+                var offset = $('.court').offset();
+                var x = ev.clientX - offset.left;
+                var y = ev.clientY - offset.top;
+
+                console.log(x, y);
+                 $('#bubble').css('top',y+70);
+                 $('#bubble').css('left',x+10);
+                 $('.boxB').css('opacity','0.5');
+                 $('.boxB').each(function (){
+                    this.style.pointerEvents = 'none'; 
+                 });
+                 if($scope.second == true){
+                    
+                    
+                    $scope.restore();
+                    
+                    
+                 }
+                 
+            });
+            
+        
+    }
+
+    $scope.restore = function(){
+        $timeout(function(){
+            $('.court > div').css('background-color', 'white');
+            $('.boxA').css('opacity','1');
+            $('.boxB').css('opacity','1');
+            $('.boxA').each(function (){
+                this.style.pointerEvents = 'auto'; 
+             });
+             $('.boxB').each(function (){
+                this.style.pointerEvents = 'auto'; 
+             });
+             $scope.isCoordinateSelected = false;
+             $scope.isScoreSelected = false;
+             $scope.first = false;
+             $scope.isSet = false;
+             $scope.second = false;
+             var ele = $(".activePlayer");
+             ele.addClass("activePlayer");
+        },4000);
+
+        
+    }
+
 
     $scope.teamApointsUpdate = function () {
+        console.log("updating");
+        $scope.isScoreSelected = true;
+        $('.court > div').css('background-color', 'yellow');
+        $scope.getCourtCoordinates ();
+        
 
         $scope.teamApoints = $scope.teamApoints + 1;
         $scope.activePlayerId = $(".activePlayer").attr('id');
@@ -154,7 +254,7 @@ angular.module('badminton').controller('matchPageController', function ($scope, 
             window.location.reload();
         }
         $scope.courtDisable = true;
-        $('.court > div').css('cursor', 'not-allowed');
+        
         if ($scope.receiveTeam != "teamA") {
             $scope.swapA();
         }
@@ -177,11 +277,15 @@ angular.module('badminton').controller('matchPageController', function ($scope, 
 
             }
         }
+        
 
 
     }
     $scope.teamBpointsUpdate = function () {
-
+        $scope.isScoreSelected = true;
+        $('.court > div').css('background-color', 'yellow');
+        
+        $scope.getCourtCoordinates ();
         $scope.teamBpoints = $scope.teamBpoints + 1;
         $scope.activePlayerId = $(".activePlayer").attr('id');
         $scope.undoDisable = false;
